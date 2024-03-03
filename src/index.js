754924c811867b0c1f347de8c8132f53eba8a2a4
thinhlaptrinhk14 <thinhvpk14@gmail.com>
2024-03-03 22:54:43 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json())
+
 //Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
@@ -19,9 +25,22 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log(path.join(__dirname, 'resources/views'));
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
