1a78e5e51e766ce335be382d64e17a8c01a9291f
thinhlaptrinhk14 <thinhvpk14@gmail.com>
2024-03-04 17:15:29 +0700

    routes and controller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -25,22 +26,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log(path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.render('search');
-});
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
