e2acdd106c754ff94edfe4484942633b57f9589f
thinhlaptrinhk14 <thinhvpk14@gmail.com>
2024-03-07 23:58:20 +0700

    create course

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,19 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
   //[GET] /news
-  home(req, res) {
+  home(req, res, next) {
 
     Course.find({})
       .then(courses => {
-        res.json(courses);
+        res.render('home', {
+          courses: multipleMongooseToObject(courses)
+        });
+
       })
-      .catch(error => {
-        res.status(400).json({ error: 'ERROR' });
-      });
-
+      .catch(next);
   }
-
   //[GET] /news/:search
   search(req, res) {
     res.render('search');
